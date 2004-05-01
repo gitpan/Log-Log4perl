46350adb21c495e1cea6acfc489bee9d103a6d9c
Michael Schilli <m@perlmeister.com>
2004-05-01 02:08:02 +0000

    Import of MSCHILLI/Log-Log4perl-0.44 from CPAN.
    gitpan-cpan-distribution: Log-Log4perl
    gitpan-cpan-version:      0.44
    gitpan-cpan-path:         MSCHILLI/Log-Log4perl-0.44.tar.gz
    gitpan-cpan-author:       MSCHILLI
    gitpan-cpan-maturity:     released

--- lib/Log/Log4perl.pm
+++ lib/Log/Log4perl.pm
@@ -14,7 +14,7 @@ use Log::Log4perl::Appender;
 
 use constant _INTERNAL_DEBUG => 1;
 
-our $VERSION = '0.43';
+our $VERSION = '0.44';
 
    # set this to '1' if you're using a wrapper
    # around Log::Log4perl
@@ -610,6 +610,7 @@ C<Log::Log4perl> already comes with a standard set of appenders:
     Log::Log4perl::Appender::Socket
     Log::Log4perl::Appender::DBI
     Log::Log4perl::Appender::Synchronized
+    Log::Log4perl::Appender::RRDs
 
 to log to the screen, to files and to databases. 
 
